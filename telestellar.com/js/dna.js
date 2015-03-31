d9922b484a19d8c9d652236b8f23a9eba9c21f97
Kayla Lewis <kaylalewis322@gmail.com>
2015-03-30 21:56:42 -0500

    dna

--- telestellar.com/js/dna.js
+++ telestellar.com/js/dna.js
@@ -1,12 +1,46 @@
-var A = 42;
-var T = 48;
-var C = 30;
-var G = 50;
-var order = [A, C, G, T];
+// var A = 42;
+// var T = 48;
+// var C = 30;
+// var G = 50;
+// var order = [A, C, G, T];
 
-function formula() {
+// function formula() {
 
-order[2]*3=x;
-console.log(x)
+// order[2]*3=x;
+// console.log(x)
+
+// }
+
+$('canvas')
+// Draw a circle
+.drawArc({
+  layer: true,
+  groups: ['circles'],
+  fillStyle: '#c33',
+  x: 100, y: 100,
+  radius: 50
+})
+// Draw another circle
+.drawArc({
+  layer: true,
+  groups: ['circles'],
+  fillStyle: '#36c',
+  x: 220, y: 100,
+  radius: 50
+})
+// Animate all layers in the group 'circles'
+.animateLayerGroup('circles', {
+  y: 200
+}, 500);
+
+function initialize() {
+
+document.getElementById("buttonFlash").style.display = "none";
+
+var first = document.getElementById("firstStrand");
+first.id = "firstStrandAnimate";
+
+var second = document.getElementById("otherStrands");
+second.id = "secondStrandFade";
 
 }
